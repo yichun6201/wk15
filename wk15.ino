7904166d611937de7180caebec7cb733eb380871
yichunchun <39977904+yichunchun@users.noreply.github.com>
2018-06-06 11:28:25 +0800

    Add files via upload

---
+++ wk15.ino
@@ -0,0 +1,18 @@
+
+
+void setup() {
+
+Serial.begin(9600);}
+
+void loop() {
+int sensorRead = analogRead(A0);
+{
+    if(sensorRead< 100){sensorRead=0;}
+  else if(sensorRead>257){sensorRead=257;}
+  int ledDimming = map(sensorRead,0, 257, 0, 255);
+  analogWrite(3,ledDimming);
+  Serial.println(ledDimming);
+  delay(2);
+}
+
+}
